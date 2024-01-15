b9398697b7e69f1ee951c91e6dcff5b7f9b2cfc5
amadorcf <amadorcf@outlook.com>
2024-01-15 12:32:46 +0100

    Directiva ngIf

--- 05_learning-angular/src/app/zapatillas/zapatillas.component.ts
+++ 05_learning-angular/src/app/zapatillas/zapatillas.component.ts
@@ -13,8 +13,13 @@ export class ZapatillasComponent implements OnInit{
     constructor(){
       this.zapatillas = [
         new Zapatilla('Adidas Classic', 'Adidas', 'Blanco', 69.99, true),
+        new Zapatilla('Adidas Ultra Boost', 'Adidas', 'Negro', 120.00, true),
         new Zapatilla('Nike Air', 'Nike', 'Blanco', 89.99, true),
+        new Zapatilla('Puma Clyde Court', 'Puma', 'Rojo', 99.99, false),
+        new Zapatilla('Reebok Classic Leather', 'Reebok', 'Blanco', 79.95, true),
+
         //aqui podiamos crear mas zapatillas usando el patron de la linea anterior
+
       ];
     }
 
