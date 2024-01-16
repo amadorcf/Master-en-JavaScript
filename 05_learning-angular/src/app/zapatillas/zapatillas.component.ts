f3c566f7095db7476c7429dc47b104709147692f
amadorcf <amadorcf@outlook.com>
2024-01-16 19:35:32 +0100

    Eventos: focus, blur, keyup

--- 05_learning-angular/src/app/zapatillas/zapatillas.component.ts
+++ 05_learning-angular/src/app/zapatillas/zapatillas.component.ts
@@ -58,5 +58,18 @@ export class ZapatillasComponent implements OnInit{
       //delete this.marcas[index];
       this.marcas.splice(index, 1);
     }
-    
+
+
+    // Evento focus: ocurre cuando un elemento se enfoca. El evento onfocus se usa con mayor frecuencia con  <input>, <select> y <a>
+    // Evento blur: se usa para quitar el foco de un elemento.
+    onBlur(){
+      console.log("Hemos salido del input")
+    }
+
+    // Evento keyup.enter
+    onKeyUp(){
+      console.log("Se ha pulsado la tecla ENTER")
+      alert("La marca es:  " + this.mi_marca)
+    }
+
 }
