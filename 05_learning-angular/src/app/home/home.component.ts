840ce90f07c9d322949ee5e5704fe8408461e9c0
amadorcf <amadorcf@outlook.com>
2024-01-18 19:03:53 +0100

    Directivas ngTemplate

--- 05_learning-angular/src/app/home/home.component.ts
+++ 05_learning-angular/src/app/home/home.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+		selector: 'app-home',
+		templateUrl: './home.component.html',
+		styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+		public identificado: boolean;
+
+		constructor(){
+        this.identificado = false;
+		}
+
+		setIdentificado(){
+        this.identificado = true;
+		}
+
+    unsetIdentificado(){
+        this.identificado = false;
+		}
 
 }
