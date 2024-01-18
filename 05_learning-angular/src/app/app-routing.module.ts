f5331a7a0e977558c07724b61fb19f587d7cc109
amadorcf <amadorcf@outlook.com>
2024-01-18 11:33:05 +0100

    Configuracion del routing

--- 05_learning-angular/src/app/app-routing.module.ts
+++ 05_learning-angular/src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
-import { NgModule } from '@angular/core';
+// Importar modulos del router de Angular
+import { NgModule, ModuleWithProviders } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// Importar componentes
+import { HomeComponent } from './home/home.component';
+import { VideojuegoComponent } from './videojuego/videojuego.component';
+import { ZapatillasComponent } from './zapatillas/zapatillas.component';
+import { CursosComponent } from './cursos/cursos.component';
 
+
+// Array de rutas
+const appRoutes: Routes = [
+  {path:'', component: HomeComponent},
+  {path:'videojuegos', component: VideojuegoComponent},
+  {path:'zapatillas', component: ZapatillasComponent},
+  {path:'cursos', component: CursosComponent},
+  {path: '**', component: HomeComponent}
+];
+
+// Exportar modulo del router
+/*
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+*/
+export const appRoutingProviders: any[] = [];
+export const routing: ModuleWithProviders<any> = RouterModule.forRoot(appRoutes);
