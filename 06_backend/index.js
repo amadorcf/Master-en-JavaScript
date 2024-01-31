c960b40c3e63e159ff6740c3c28cf588d4751506
amadorcf <amadorcf@outlook.com>
2024-01-31 19:07:55 +0100

    Actualizado mensaje de la conexion al servidor

--- 06_backend/index.js
+++ 06_backend/index.js
@@ -6,12 +6,12 @@ var port = 3700;
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/portfolio')
-    .then( () => {
+    .then(() => {
         console.log("Conectado correctamente a base de datos Portfolio en MongoDB")
 
         // Creacion del SERVIDOR
         app.listen(port, () =>{
-            console.log("Servidor corriendo correctamente en la url: localhost:3700")
+            console.log("Servidor corriendo correctamente en la url: localhost:"+ port)
         });
 
     })
