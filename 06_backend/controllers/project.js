f61d41b7933ed6b51d8467b7789884d985929eb5
amadorcf <amadorcf@outlook.com>
2024-02-01 16:55:50 +0100

    Implementacion metodo updateProject

--- 06_backend/controllers/project.js
+++ 06_backend/controllers/project.js
@@ -59,15 +59,61 @@ var controller = {
             message:'Metodo save-project'
         })
         */
-    }
+    },
 
     // READ
+    getProject: async function (req, res) {
+        var projectId = req.params.id;
+      
+        // Si no existe proyecto...
+        if (projectId == null) return res.status(404).send({message: "El proyecto no existe"});
+      
+        // Si existe, devolverlo
+        try {
+          const project = await Project.findById(projectId);
+          if (!project) return res.status(404).send({message: "No se ha encontrado el proyecto"});
+          return res.status(200).send({project});
+        } catch (err) {
+          return res.status(500).send({message: "Error al devolver los datos"});
+        }
+    },
+
+    getProjects: function(req, res){
+
+        Project.find({}).sort('-year')
+            .then((projects)=>{
+                if(!projects) return res.status(404).send({message: "No hay projectos que mostrar..."});
     
+                return res.status(200).send({projects});
+            })
+            .catch((err)=>{
+                if(err) return res.status(500).send({message: "Error al devolver los datos"});
+            })
+    },
 
     // UPDATE
+    updateProject: function(req, res){
+        var projectId = req.params.id;
 
+        var update_params = req.body;
+
+        // Si no existe proyecto...
+        if (projectId == null) return res.status(404).send({message: "El proyecto no existe"});
+
+        Project.findByIdAndUpdate(projectId, update_params, {new:true})
+        .then((projectUpdated)=>{
+            return res.status(200).send({
+                project: projectUpdated
+            })
+        })
+        .catch(() => {
+            return res.status(404).send({message: "Imposible actualizar. No se ha encontrado el proyecto"});
+        })
+    }
 
     // DELETE
+
+
 }
 
 module.exports = controller;
